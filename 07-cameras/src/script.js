ef5914191c3b867c36db2347ecf7220b5c306095
Tom Metcalf <intermet@gmail.com>
2023-08-22 22:15:47 +0100

    add 07 camera controls damping

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Cursor
 const cursor = {
@@ -56,6 +57,12 @@ camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+// controls.target.y = 2
+// controls.update()
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -66,16 +73,19 @@ renderer.setSize(sizes.width, sizes.height);
 const clock = new THREE.Clock();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+  //   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
   //   mesh.rotation.y = elapsedTime;
 
-  // Update camera
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  camera.position.y = cursor.y * 5;
-  camera.lookAt(mesh.position);
+  //   // Update camera
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.y = cursor.y * 5;
+  //   camera.lookAt(mesh.position);
+
+// Update controls
+controls.update()
 
   // Render
   renderer.render(scene, camera);
