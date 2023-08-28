4c903e8a8e8fbb265bbd0bc781f6f1ef965e5ec2
Tom Metcalf <intermet@gmail.com>
2023-08-28 12:07:46 +0100

    add 11 texture filters

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -24,7 +24,7 @@ const loadingManager = new THREE.LoadingManager();
 // };
 
 const textureLoader = new THREE.TextureLoader(loadingManager);
-const colorTexture = textureLoader.load('/textures/door/color.jpg');
+const colorTexture = textureLoader.load('/textures/minecraft.png');
 const alphaTexture = textureLoader.load('/textures/door/alpha.jpg');
 const heightTexture = textureLoader.load('/textures/door/height.jpg');
 const normalTexture = textureLoader.load('/textures/door/normal.jpg');
@@ -40,10 +40,15 @@ const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
 // colorTexture.offset.x = 0.5
 // colorTexture.offset.y = 0.5
 
-colorTexture.rotation = Math.PI * 0.25
+// colorTexture.rotation = Math.PI * 0.25
 
-colorTexture.center.x = 0.5
-colorTexture.center.y = 0.5
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+// colorTexture.generateMipmaps = false
+// colorTexture.minFilter = THREE.NearestFilter
+colorTexture.magFilter = THREE.NearestFilter
+// colorTexture.magFilter = THREE.LinearFilter
 
 
 /**
