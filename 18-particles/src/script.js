bc26e44b38cb0491427e21ee38f5459cbeefb8db
Tom Metcalf <intermet@gmail.com>
2023-09-14 20:32:49 +0100

    add 19 particles animate wave particles

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -140,8 +140,18 @@ const tick = () => {
   // particles.position.y = -elapsedTime * 0.02;
 
   // Rotating particles
-  particles.rotation.y = elapsedTime * 0.02;
-  
+  // particles.rotation.y = elapsedTime * 0.02;
+
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+
+    const x = particlesGeometry.attributes.position.array[i3]
+
+    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x);
+  }
+
+  particlesGeometry.attributes.position.needsUpdate = true;
+
   // Update controls
   controls.update();
 
