4be6c9366d5ce12c0dd6ced468eea233cfc88c2a
Tom Metcalf <intermet@gmail.com>
2023-09-16 16:59:19 +0100

    add 19 galaxy generator straight line particles

--- 19-galaxy-generator/src/script.js
+++ 19-galaxy-generator/src/script.js
@@ -32,6 +32,7 @@ const parameters = {};
 
 parameters.count = 100000;
 parameters.size = 0.01;
+parameters.radius = 5
 
 let geometry = null
 let material = null
@@ -55,9 +56,11 @@ if (points !== null) {
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3;
 
-    positions[i3] = (Math.random() - 0.5) * 3;
-    positions[i3 + 1] = (Math.random() - 0.5) * 3;
-    positions[i3 + 2] = (Math.random() - 0.5) * 3;
+    const radius = Math.random() * parameters.radius
+
+    positions[i3] = radius
+    positions[i3 + 1] = 0
+    positions[i3 + 2] = 0
   }
 
   geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
@@ -93,6 +96,12 @@ gui
   .min(0.001)
   .max(0.1)
   .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'radius')
+  .min(0.01)
+  .max(20)
+  .step(0.01)
   .onFinishChange(generateGalaxy);
 
 /**
