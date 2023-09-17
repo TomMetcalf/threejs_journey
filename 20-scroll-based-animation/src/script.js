f7257c03c3fadb8f0da08bb9b11f72c759e00bd6
Tom Metcalf <intermet@gmail.com>
2023-09-17 14:19:53 +0100

    add 20 scroll animation fix screen fps issue

--- 20-scroll-based-animation/src/script.js
+++ 20-scroll-based-animation/src/script.js
@@ -157,17 +157,20 @@ cursor.y = event.clientY / sizes.height - 0.5
  * Animate
  */
 const clock = new THREE.Clock();
+let previousTime = 0
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
 
   // Animate camera
   camera.position.y = - scrollY / sizes.height * objectsDistance
 
-  const parallaxX = cursor.x
-  const parallaxY = - cursor.y
-  cameraGroup.position.x = parallaxX
-  cameraGroup.position.y = parallaxY
+  const parallaxX = cursor.x * 0.5
+  const parallaxY = - cursor.y * 0.5
+  cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+  cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
 
   // Animate meshes
