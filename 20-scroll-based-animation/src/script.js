9c8b64bcb58da34a3ce608fd64acb2467c8d4de5
Tom Metcalf <intermet@gmail.com>
2023-09-17 14:47:20 +0100

    add 20 scroll animation gsap rotation

--- 20-scroll-based-animation/src/script.js
+++ 20-scroll-based-animation/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import * as dat from 'lil-gui';
+import gsap from 'gsap';
 
 THREE.ColorManagement.enabled = false;
 
@@ -65,9 +66,9 @@ mesh1.position.y = -objectsDistance * 0;
 mesh2.position.y = -objectsDistance * 1;
 mesh3.position.y = -objectsDistance * 2;
 
-mesh1.position.x = 2
-mesh2.position.x = - 2
-mesh3.position.x = 2
+mesh1.position.x = 2;
+mesh2.position.x = -2;
+mesh3.position.x = 2;
 
 scene.add(mesh1, mesh2, mesh3);
 
@@ -77,28 +78,33 @@ const sectionMeshes = [mesh1, mesh2, mesh3];
  * Particles
  */
 // Geometry
-const particlesCount = 200
-const positions = new Float32Array(particlesCount * 3)
+const particlesCount = 200;
+const positions = new Float32Array(particlesCount * 3);
 
-for(let i = 0; i < particlesCount ; i++) {
-    positions[i * 3] = (Math.random() - 0.5) * 10
-    positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
-    positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+for (let i = 0; i < particlesCount; i++) {
+  positions[i * 3] = (Math.random() - 0.5) * 10;
+  positions[i * 3 + 1] =
+    objectsDistance * 0.5 -
+    Math.random() * objectsDistance * sectionMeshes.length;
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
 }
 
-const particlesGeomtry = new THREE.BufferGeometry()
-particlesGeomtry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+const particlesGeomtry = new THREE.BufferGeometry();
+particlesGeomtry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positions, 3)
+);
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
-    color: parameters.materialColor,
-    sizeAttenuation: true,
-    size: 0.03
-})
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03,
+});
 
 // Points
-const particles = new THREE.Points(particlesGeomtry, particlesMaterial)
-scene.add(particles)
+const particles = new THREE.Points(particlesGeomtry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Light
@@ -136,8 +142,8 @@ window.addEventListener('resize', () => {
  */
 
 // Group
-const cameraGroup = new THREE.Group()
-scene.add(cameraGroup)
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
 
 // Base camera
 const camera = new THREE.PerspectiveCamera(
@@ -164,47 +170,63 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Scroll
  */
 let scrollY = window.scrollY;
+let currentSection = 0;
 
 window.addEventListener('scroll', () => {
   scrollY = window.scrollY;
+
+  const newSection = Math.round(scrollY / sizes.height);
+
+  if (newSection != currentSection) {
+    currentSection = newSection;
+
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: 'power2.inOut',
+      x: '+=6',
+      y: '+=3',
+      z: '+=1.5',
+    });
+  }
 });
 
 /**
  * Cursor
  */
-const cursor = {}
-cursor.x = 0
-cursor.y = 0
+const cursor = {};
+cursor.x = 0;
+cursor.y = 0;
 
 window.addEventListener('mousemove', (event) => {
-cursor.x = event.clientX / sizes.width - 0.5
-cursor.y = event.clientY / sizes.height - 0.5
-})
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+});
 
 /**
  * Animate
  */
 const clock = new THREE.Clock();
-let previousTime = 0
+let previousTime = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - previousTime
-  previousTime = elapsedTime
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
 
   // Animate camera
-  camera.position.y = - scrollY / sizes.height * objectsDistance
+  camera.position.y = (-scrollY / sizes.height) * objectsDistance;
 
-  const parallaxX = cursor.x * 0.5
-  const parallaxY = - cursor.y * 0.5
-  cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
-  cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
-
+  const parallaxX = cursor.x * 0.5;
+  const parallaxY = -cursor.y * 0.5;
+  cameraGroup.position.x +=
+    (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
+  cameraGroup.position.y +=
+    (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
 
   // Animate meshes
   for (const mesh of sectionMeshes) {
-    mesh.rotation.x = elapsedTime * 0.1;
-    mesh.rotation.y = elapsedTime * 0.12;
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
   }
 
   // Render
