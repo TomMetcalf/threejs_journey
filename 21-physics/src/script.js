9d97451eb0c457a84081d16cce67abf235c9b9d8
Tom Metcalf <intermet@gmail.com>
2023-09-19 19:18:57 +0100

    add 21 physics default contact material

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -42,18 +42,30 @@ const world = new CANNON.World()
 world.gravity.set(0, - 9.82, 0)
 
 // Materials
-const concreteMaterial = new CANNON.Material('concrete')
-const plasticMaterial = new CANNON.Material('plastic')
+// const concreteMaterial = new CANNON.Material('concrete')
+// const plasticMaterial = new CANNON.Material('plastic')
 
-const concretePlasticContactMaterial = new CANNON.ContactMaterial(
-    concreteMaterial,
-    plasticMaterial,
-    {
-        friction: 0.1,
-        restitution: 0.7,
-    }
-)
-world.addContactMaterial(concretePlasticContactMaterial)
+// const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+//     concreteMaterial,
+//     plasticMaterial,
+//     {
+//         friction: 0.1,
+//         restitution: 0.7,
+//     }
+// )
+// world.addContactMaterial(concretePlasticContactMaterial)
+
+const defaultMaterial = new CANNON.Material('default');
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  }
+);
+world.addContactMaterial(defaultContactMaterial);
 
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5)
@@ -61,14 +73,14 @@ const sphereBody = new CANNON.Body({
     mass: 1,
     position: new CANNON.Vec3(0, 3, 0),
     shape: sphereShape,
-    material: plasticMaterial
+    material: defaultMaterial
 })
 world.addBody(sphereBody)
 
 // Floor
 const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body()
-floorBody.material = concreteMaterial
+floorBody.material = defaultMaterial
 floorBody.mass = 0
 floorBody.addShape(floorShape)
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(- 1, 0, 0), Math.PI * 0.5)
