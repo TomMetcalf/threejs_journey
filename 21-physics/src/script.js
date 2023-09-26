ce8ad4a3b9b0970c51d4a01e89f32a98d88174bd
Tom Metcalf <intermet@gmail.com>
2023-09-26 19:10:24 +0100

    add 21 physics create boxes

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -22,6 +22,20 @@ debugObject.createSphere = () => {
 
 gui.add(debugObject, 'createSphere')
 
+debugObject.createBox = () => {
+  createBox(
+    Math.random(), 
+    Math.random(),
+    Math.random(),
+  {
+    x: (Math.random() - 0.5) * 3,
+    y: 3,
+    z: (Math.random() - 0.5) * 3,
+  })
+};
+
+gui.add(debugObject, 'createBox');
+
 /**
  * Base
  */
@@ -200,6 +214,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const objectsToUpdate = []
 
+// Sphere
 const sphereGeometry = new THREE.SphereGeometry(1, 20, 20)
 const sphereMaterial = new THREE.MeshStandardMaterial({
   metalness: 0.3,
@@ -235,6 +250,42 @@ const createSphere = (radius, position) => {
 
 createSphere(0.5, {x: 0, y: 3, z: 0})
 
+// Box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+const createBox = (width, height, depth, position) => {
+  // Three.js Mesh
+  const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  mesh.scale.set(width, height, depth);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  // Cannon.js body
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5)
+  );
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  world.addBody(body);
+
+  // Save objects to update
+  objectsToUpdate.push({
+    mesh: mesh,
+    body: body,
+  });
+};
+
 /**
  * Animate
  */
@@ -259,6 +310,7 @@ oldElapsedTime = elapsedTime
 
     for(const object of objectsToUpdate) {
         object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
     }
 
     // Update controls
